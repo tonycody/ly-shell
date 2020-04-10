7da1acb7287e29330be9acae8e9caeb609575c77
XS <tonycody@qq.com>
2020-04-10 11:20:14 +0800

    fix
    fix

--- dump.sh
+++ dump.sh
@@ -12,7 +12,7 @@ if [ ! -d "/data/logs/skynet-${DAOKEAPPUK}/dump" ]; then
   mkdir /data/logs/skynet-${DAOKEAPPUK}/dump
 fi
 pid=$(ps -ef | grep 'java' | grep -v grep | awk '{print $2}')
-fileName=/data/logs/skynet-${DAOKEAPPUK}/dump/`date +%Y%m%d`$RANDOM.phrof
+fileName=/data/logs/skynet-${DAOKEAPPUK}/dump/`date +%Y%m%d`$RANDOM.hprof
 jmap -dump:format=b,file=$fileName $pid
 tar -zcvf $fileName.tar.gz $fileName
 rm -rf $fileName
